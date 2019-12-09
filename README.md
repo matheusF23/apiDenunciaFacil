2b7216f362b09b8539099809ac3f7751aa4b3884
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-12-08 22:01:38 -0300

    Criação do createOcorrencia e readOcorrencia.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Denúncia Fácil - API
 
 ## Comandos de acesso
+### Comandos para Usuário
 **Cadastrar um usuário:**
 
 http://localhost:5000/user?name=nome&cpf=00000000000&username=username&email=email@gmail.com&senha=senha
@@ -18,3 +19,14 @@ http://localhost:5000/user/update/00000000000/?name=nome&cpf=00000000000&usernam
 É preciso passar o cpf após delete/
 
 http://localhost:5000/user/delete/00000000000
+
+### Comandos para Ocorrência
+**Cadastrar uma ocorrência:**
+
+Aqui não foi passado alguns campos, como os de localização da denuncia e hora. Mas pode ser passado colocando &nomeDoParametro .. Ex: ...&hora=hh:mm:ss
+
+http://localhost:5000/ocorrencia?cpf_usuario=88888888888&placa=ABC0000&tipo_ocorrencia=Estacionamento+proibido&descricao=Motorista+estacionou+na+frente+da+minha+garagem&data=2019-12-07
+
+**Visualizar ocorrências:**
+
+http://localhost:5000/ocorrencia/select
--- api_flask.py
+++ api_flask.py
@@ -1,13 +1,14 @@
 from flask import Flask, escape, request, jsonify
-from resource import Usuario
+from resource import Usuario, Ocorrencia
 
 app = Flask(__name__)
 
-## Rotas para Usuário ##
 @app.route('/')
 def home():
     return "Seja Bem Vindo à API Denúncia Fácil"
 
+## Rotas para Usuário ##
+# Criar Usuário
 @app.route('/user')
 def user():
     usuario = Usuario()
@@ -27,12 +28,14 @@ def user():
             }
     return usuario.createUser(dados)
 
+# Visualizar usuários
 @app.route('/user/select')
 def selectUser():
     usuario = Usuario()
     saida = usuario.readUser() 
     return jsonify(saida), 200
 
+# Atualizar Usuário
 @app.route('/user/update/<string:_cpf>/')
 def updateUser(_cpf):
     usuario = Usuario()
@@ -53,10 +56,54 @@ def updateUser(_cpf):
    
     return usuario.updateUser(_cpf, dados)
 
+# Deletar Usuário
 @app.route('/user/delete/<string:_cpf>')
 def deleteUser(_cpf):
     usuario = Usuario()
     return usuario.deleteUser(_cpf)
+
+
+## Rotas para Ocorrência ##
+# Criar Ocorrência
+@app.route('/ocorrencia')
+def ocorrencia():
+    ocorrencia = Ocorrencia()
+    
+    cpf_usuario = request.args.get("cpf_usuario")
+    placa = request.args.get("placa")
+    titulo_ocorrencia = request.args.get("titulo_ocorrencia")
+    tipo_ocorrencia = request.args.get("tipo_ocorrencia")
+    descricao = request.args.get("descricao")
+    data = request.args.get("data")
+    hora = request.args.get("hora")
+    bairro = request.args.get("bairro")
+    rua = request.args.get("rua")
+    cidade = request.args.get("cidade")
+    estado = request.args.get("estado")
+
+
+    dados = {
+                "cpf_usuario": cpf_usuario,
+                "placa": placa,
+                "titulo_ocorrencia": titulo_ocorrencia,
+                "tipo_ocorrencia": tipo_ocorrencia,
+                "descricao": descricao,
+                "data": data,
+                "hora": hora,
+                "bairro": bairro,
+                "rua": rua,
+                "cidade": cidade,
+                "estado": estado
+            }
+    return ocorrencia.createOcorrencia(dados)
+
+# Visualizar Ocorrências
+@app.route('/ocorrencia/select')
+def selectOcorrencia():
+    ocorrencia = Ocorrencia()
+    saida = ocorrencia.readOcorrencia()
+    return jsonify(saida), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
